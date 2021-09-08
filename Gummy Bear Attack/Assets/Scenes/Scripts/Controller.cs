7a1b69aa76bab9303fdeb621bdeafe65e4827ec8
TechnoM4ncer <kinodekirakashi@live.com>
2021-09-08 05:09:40 -0500

    Add functionality for looping the bounds of the level

--- Gummy Bear Attack/Assets/Scenes/Scripts/Controller.cs
+++ Gummy Bear Attack/Assets/Scenes/Scripts/Controller.cs
@@ -4,8 +4,36 @@ public class Controller : MonoBehaviour
 {
     public Rigidbody rb;
     public float moveSpeed = 25f;
+    public float xRange = 13f;
+    public float zRange = 13f;
+    Vector3 pos;
+    Vector3 newPos;
+
+    void Start () {
+        
+    }
+
     void FixedUpdate()
     {
+        pos = transform.position;
+
+        if (pos.x >= xRange) {
+            newPos = new Vector3 ( -pos.x , pos.y , pos.z );
+            transform.position = newPos;
+        }
+        if (pos.x <= -xRange) {
+            newPos = new Vector3 ( -pos.x , pos.y , pos.z );
+            transform.position = newPos;
+        }
+        if (pos.z >= zRange) {
+            newPos = new Vector3 ( pos.x , pos.y , -pos.z );
+            transform.position = newPos;
+        }
+        if (pos.z <= -zRange) {
+            newPos = new Vector3 ( pos.x , pos.y , -pos.z );
+            transform.position = newPos;
+        }
+
         if (Input.GetKey("w")) {
             rb.AddForce( 0 , 0 , moveSpeed * Time.deltaTime , ForceMode.VelocityChange );
         }
